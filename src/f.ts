bbe7c5de702c6924a13e3f6de27c6ee5cbc59c29
Vincent <thisoneis4business@gmail.com>
2022-10-19 10:00:40 +1300

    feat: New API for f

--- src/f.ts
+++ src/f.ts
@@ -10,7 +10,7 @@
  */
 export class F<X> {
   private constructor(
-    private wrapped: X
+    private _x: X
   ){}
 
   /**
@@ -19,11 +19,11 @@ export class F<X> {
    * @returns A new F with the return value of the function
    */
   private pipe<Y>(g: (x: X) => Y | F<Y>): F<Y> {
-    const y = g(this.wrapped);
+    const y = g(this._x);
     
     // If not an object "in" throws an error, so casting is safe
     try {
-      if ("wrapped" in y) {
+      if ("_x" in y) {
         return y;
       }
       return new F(y);
@@ -37,26 +37,29 @@ export class F<X> {
    * @param g Function to pipe the value into
    * @returns A new F with the return value of the function
    */
-  public next<Y>(g: (x: X) => Y | F<Y>): F<Y> {
-    return this.pipe(g);
-  }
-
-  /**
-   * Pipe the value into a given function and put the result into another F
-   * @param g Function to pipe the value into
-   * @returns A new F with the return value of the function
-   */
   public f<Y>(g: (x: X) => Y | F<Y>): F<Y> {
     return this.pipe(g);
   }
 
+
   /**
-   * Pipe a predicate
-   * @param p Predicate to apply the value
-   * @returns The result of the predicate
+   * Asynchronously pipe the value into a given function and put the result into another F
+   * @param g Async / sync function to pipe the value into
+   * @returns A new F with the return value of the function
    */
-  public is(p: (x: X) => boolean): boolean {
-    return p(this.wrapped);
+  public a<Y>(g: (x: Awaited<X>) => Y | F<Y> | Promise<Y | F<Y>>): F<Promise<Y>> {
+    return this.pipe(async prom => {
+      const x = await prom;
+      const y = await g(x);
+      try {
+        if ("_x" in y) {
+          return y.y();
+        }
+        return y;
+      } catch {
+        return y as Y;
+      }
+    });
   }
 
   /**
@@ -65,16 +68,7 @@ export class F<X> {
    * @returns The result of the predicate
    */
   public p(p: (x: X) => boolean): boolean {
-    return this.is(p);
-  }
-
-
-  /**
-   * Pipe a void function to the wrapped value
-   * @param z The void function to pipe
-   */
-  public void(z: (x: X) => void): void {
-    z(this.wrapped);
+    return p(this._x);
   }
 
   /**
@@ -82,38 +76,24 @@ export class F<X> {
    * @param z The void function to pipe
    */
   public z(z: (x: X) => void): void {
-    this.void(z);
+    z(this._x);
   }
 
   /**
-   * Get the value from F
+   * Union 2 F's
+   * @param g Another F to combine with
+   * @returns A union of both F
    */
-  public get value(): X {
-    return this.wrapped;
-  }
-
-  /**
-   * End the piping and the value from F
-   * @returns The value in the F
-   */
-  public x(): X {
-    return this.wrapped;
+  public u<Y>(g: F<Y>): F<[X, Y]> {
+    return new F([this._x, g._x]);
   }
 
   /**
    * End the piping and the value from F
    * @returns The value in the F
    */
-  public end(): X {
-    return this.value;
-  }
-
-  /**
-   * Get the value as a flattened Promise
-   * @returns The value as a promise
-   */
-  public async promise(): Promise<Awaited<X>> {
-    return await this.wrapped;
+  public y(): X {
+    return this._x;
   }
 
   /**
@@ -121,7 +101,7 @@ export class F<X> {
    * @returns JSON value for F
    */
   public toJSON(): X {
-    return this.wrapped;
+    return this._x;
   }
 
   /**
@@ -129,7 +109,7 @@ export class F<X> {
    * @returns String format for F
    */
   public toString(): string {
-    return `${this.wrapped}`
+    return `${this._x}`
   }
 
   /**
@@ -137,42 +117,7 @@ export class F<X> {
    * @param x The initial value
    * @returns A new F with the value
    */
-  public static new<X>(x: X): F<X> {
+  public static x<X>(x: X): F<X> {
     return new F(x);
-  }
-
-  /**
-   * Higher order function that allow the function to play with the awaited value
-   * @param g Function (async or not) that process the awaited value in the F
-   * @returns A function that can be use in the pipe for a Promise
-   */
-  public static async<X, Y>(g: (x: Awaited<X>) => Y | Promise<Y>): (x: Promise<X>) => Promise<Y> {
-    return async (prom) => {
-      const x = await prom;
-      return g(x);
-    };
-  }
-}
-
-/**
- * Create a new F to start the piping
- * @param x The initial value
- * @returns A new F with the value
- */
-export function f<X>(x: X): F<X> {
-  return F.new(x);
-}
-
-export namespace f {
-  /**
-   * Higher order function that allow the function to play with the awaited value
-   * @param g Function (async or not) that process the awaited value in the F
-   * @returns A function that can be use in the pipe for a Promise
-   */
-  export function a<X, Y>(g: (x: Awaited<X>) => Y | Promise<Y>): (x: Promise<X>) => Promise<Y> {
-    return async (prom) => {
-      const x = await prom;
-      return g(x);
-    };
   }
 }
